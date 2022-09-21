cf9115c0a8a62741a3259350b99c535910c443f8
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-09-21 19:27:49 +0530

    deliberately failing a test to check CI

--- projects/calculator/calculator.test.py
+++ projects/calculator/calculator.test.py
@@ -4,6 +4,6 @@ from projects.calculator.calculator import Calculator
 class TestSum(unittest.TestCase):
   def test_sum(self):
     calculator = Calculator()
-    self.assertEqual(calculator.add(1,2),3)
+    self.assertEqual(calculator.add(1,2),4)
 if __name__ == "__main__":
   unittest.main()
